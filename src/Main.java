421b57ff3b581114c9c3ee91970f62ac2dd8a8da
mashupmark <mashupmark@kabelbw.de>
2021-05-06 11:29:26 +0200

    added RobotSim example

--- src/Main.java
+++ src/Main.java
@@ -1,31 +1,46 @@
 import ch.aplu.robotsim.*;
+import ch.aplu.util.QuitPane;
 
 public class Main {
+    static {
+        RobotContext.setLocation(10, 10);
+        RobotContext.setStartDirection(5);
+        RobotContext.setStartPosition(100, 240);
+        RobotContext.useObstacle(RobotContext.channel);
+    }
 
-    public static void main(String[] args) {
+    static public void main(String[] args) {
         LegoRobot robot = new LegoRobot();
-        Motor motA = new Motor(MotorPort.A);
-        Motor motB = new Motor(MotorPort.B);
-        robot.addPart(motA);
-        robot.addPart(motB);
+        Gear gear = new Gear();
+        robot.addPart(gear);
+        TouchSensor ts1 = new TouchSensor(SensorPort.S1); // right sensor
+        TouchSensor ts2 = new TouchSensor(SensorPort.S2); // left sensor
+        robot.addPart(ts1);
+        robot.addPart(ts2);
+        gear.forward();
+        while (!QuitPane.quit()) {
+            boolean t1 = ts1.isPressed();
+            boolean t2 = ts2.isPressed();
 
-        motA.forward();
-        motB.forward();
-        Tools.delay(2000);
-
-        motA.stop();
-        Tools.delay(2000);
-
-        motB.stop();
-        Tools.delay(2000);
-
-        motA.backward();
-        motB.forward();
-        Tools.delay(2000);
-
-        motB.backward();
-        Tools.delay(2000);
-
+            if (t1 && t2) {
+                gear.backward(500);
+                gear.left(400);
+                gear.forward();
+            } else {
+                if (t1) {
+                    gear.backward(500);
+                    gear.left(400);
+                    gear.forward();
+                } else {
+                    if (t2) {
+                        gear.backward(500);
+                        gear.right(100);
+                        gear.forward();
+                    }
+                }
+            }
+            Tools.delay(20);
+        }
         robot.exit();
     }
 }
